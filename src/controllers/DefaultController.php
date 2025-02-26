a4a4991ce26eb9b285366271bb5ea89997709121
Frederik Lux <f.lux@phytec.de>
2025-02-26 15:01:45 +0100

    add default controller for frontend view

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace report\src\controllers;
+
+use Exception;
+use report\src\modules\ReportModel;
+use Yii;
+use yii\web\Controller;
+use yii\web\Response;
+
+class DefaultController extends Controller
+{
+	public function actionView()
+	{
+		Yii::$app->response->format = Response::FORMAT_HTML;
+		$request = Yii::$app->request;
+
+		$model = new ReportModel();
+		if ($model->load($request->post())) {
+			$verification = $model->verify();
+			if ($verification) {
+				Yii::$app->session->setFlash('error', $verification);
+			} else {
+				try {
+					$model->createReport();
+				} catch (Exception $e) {
+					Yii::$app->session->setFlash('error', $e->getMessage());
+				}
+			}
+		}
+
+		return $this->render('view');
+	}
+
+}
--- src/controllers/ReportController.php
+++ src/controllers/ReportController.php
@@ -1,36 +1,13 @@
 <?php
 
-namespace report\controllers;
+namespace report\src\controllers;
 
-use Exception;
 use report\src\modules\Report;
-use report\src\modules\ReportModel;
 use Yii;
-use yii\web\Controller;
-use yii\web\Response;
+use yii\rest\Controller;
 
 class ReportController extends Controller
 {
-	public function actionView()
-	{
-		Yii::$app->response->format = Response::FORMAT_HTML;
-		$request = Yii::$app->request;
-
-		$model = new ReportModel();
-		if ($model->load($request->post())) {
-			$verification = $model->verify();
-			if ($verification) {
-				Yii::$app->session->setFlash('error', $verification);
-			} else {
-				try {
-					$model->createReport();
-				} catch (Exception $e) {
-					Yii::$app->session->setFlash('error', $e->getMessage());
-				}
-			}
-		}
-	}
-
 	public function actionCreate()
 	{
 		// Beispiel-Daten
